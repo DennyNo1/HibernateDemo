00cbe9fd86a65e357bc5526ca2a2d721d524596f
Denny <zht2041359@vip.qq.com>
2023-07-11 16:05:28 +0800

    first day and couldn't find hibernate framework on idea. I did all configuration manually.

---
+++ demo1/src/main/java/entity/News.java
@@ -0,0 +1,20 @@
+package entity;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.util.Date;
+
+/**
+ * User: Denny
+ * Date: 2023/7/11 15:47
+ * Description
+ */
+@Data
+public class News
+{
+    private Integer id;
+    private String title;
+    private String author;
+    private Date date;
+}
